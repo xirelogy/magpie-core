5c0af5b7741d2184d112e4d11a3670c121568d82
jxtou <jxtou@xirelogy.com>
2022-12-29 16:51:20 +0800

    Allow ApiEditor to specify hidden keys

--- src/Controllers/Strategies/ApiEditor.php
+++ src/Controllers/Strategies/ApiEditor.php
@@ -4,6 +4,7 @@ namespace Magpie\Controllers\Strategies;
 
 use Magpie\Codecs\Parsers\Parser;
 use Magpie\Exceptions\ArgumentException;
+use Magpie\Exceptions\MissingArgumentException;
 use Magpie\General\Sugars\Excepts;
 
 /**
@@ -15,6 +16,34 @@ class ApiEditor extends ApiModifier
      * @var bool If any changes registered
      */
     protected bool $isChanged = false;
+    /**
+     * @var array<string, string|int> Keys hidden from access
+     */
+    protected array $hiddenKeys = [];
+
+
+    /**
+     * Specify keys to be hidden
+     * @param string|int $key
+     * @return $this
+     */
+    public function withHiddenKey(string|int $key) : static
+    {
+        $this->hiddenKeys["$key"] = $key;
+        return $this;
+    }
+
+
+    /**
+     * Specify keys to be no longer hidden
+     * @param string|int $key
+     * @return $this
+     */
+    public function withHiddenKeyRemoved(string|int $key) : static
+    {
+        unset($this->hiddenKeys["$key"]);
+        return $this;
+    }
 
 
     /**
@@ -28,6 +57,7 @@ class ApiEditor extends ApiModifier
      */
     public function whenRequires(string|int $key, ?Parser $parser, callable $onValue) : bool
     {
+        if ($this->isKeyHidden($key)) return false;
         if (!$this->parserHost->has($key)) return false;
 
         $value = $this->parserHost->requires($key, $parser);
@@ -49,6 +79,7 @@ class ApiEditor extends ApiModifier
      */
     public function whenOptional(string|int $key, ?Parser $parser, callable $onValue) : bool
     {
+        if ($this->isKeyHidden($key)) return false;
         if (!$this->parserHost->has($key)) return false;
 
         $value = $this->parserHost->optional($key, $parser);
@@ -69,6 +100,7 @@ class ApiEditor extends ApiModifier
      */
     public function directRequires(string|int $key, ?Parser $parser = null) : mixed
     {
+        if ($this->isKeyHidden($key)) throw new MissingArgumentException($this->parserHost->fullKey($key));
         $ret = $this->parserHost->requires($key, $parser);
         $this->isChanged = true;
         return $ret;
@@ -86,11 +118,23 @@ class ApiEditor extends ApiModifier
      */
     public function directOptional(string|int $key, ?Parser $parser = null, mixed $default = null) : mixed
     {
+        if ($this->isKeyHidden($key)) return $default;
         if (!$this->parserHost->has($key)) return $default;
 
         $ret = $this->parserHost->optional($key, $parser, $default);
         $this->isChanged = true;
         return $ret;
+    }
+
+
+    /**
+     * If given key is hidden
+     * @param string|int $key
+     * @return bool
+     */
+    protected final function isKeyHidden(string|int $key) : bool
+    {
+        return array_key_exists("$key", $this->hiddenKeys);
     }
 
 
