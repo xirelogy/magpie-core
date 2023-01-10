39ae8a01012e3353e128da86e198de5a299221f3
jxtou <jxtou@xirelogy.com>
2023-01-10 10:25:38 +0800

    Add tryCastResponseAsHeaderSpecifiable() to RouteMiddleware

--- src/Routes/RouteMiddleware.php
+++ src/Routes/RouteMiddleware.php
@@ -3,10 +3,14 @@
 namespace Magpie\Routes;
 
 use Exception;
+use Magpie\General\Names\CommonHttpStatusCode;
 use Magpie\General\Traits\StaticCreatable;
+use Magpie\HttpServer\Concepts\WithHeaderSpecifiable;
 use Magpie\HttpServer\Request;
+use Magpie\HttpServer\Response;
 use Magpie\Routes\Concepts\RouteHandleable;
 use Magpie\Routes\Constants\RouteMiddlewarePriority;
+use Stringable;
 
 /**
  * Route middleware
@@ -33,5 +37,24 @@ abstract class RouteMiddleware
     public static function getPriorityWeight() : int
     {
         return RouteMiddlewarePriority::DEFAULT;
+    }
+
+
+    /**
+     * Try to cast the response to make it header specifiable
+     * @param mixed $response
+     * @return WithHeaderSpecifiable|mixed
+     */
+    protected static function tryCastResponseAsHeaderSpecifiable(mixed $response) : mixed
+    {
+        if ($response instanceof WithHeaderSpecifiable) return $response;
+        if ($response === null) return new Response('', CommonHttpStatusCode::NO_CONTENT);
+
+        // Support for string as HTML content
+        if (is_string($response)) return new Response($response);
+        if ($response instanceof Stringable) return new Response($response->__toString());
+
+        // Otherwise, 'as-is'
+        return $response;
     }
 }
