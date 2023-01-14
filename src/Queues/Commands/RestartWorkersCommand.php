7fd11921e43707428006c6e6c9d66ba4ee3ab14c
jxtou <jxtou@xirelogy.com>
2023-01-14 12:33:24 +0800

    Add output to indicate queue restarted

--- src/Queues/Commands/RestartWorkersCommand.php
+++ src/Queues/Commands/RestartWorkersCommand.php
@@ -6,6 +6,7 @@ use Magpie\Commands\Attributes\CommandDescription;
 use Magpie\Commands\Attributes\CommandSignature;
 use Magpie\Commands\Command;
 use Magpie\Commands\Request;
+use Magpie\Facades\Console;
 use Magpie\Queues\Impls\Caches\WorkersRestartedAt;
 
 #[CommandSignature('queue:restart-workers')]
@@ -18,5 +19,7 @@ class RestartWorkersCommand extends Command
     protected function onRun(Request $request) : void
     {
         WorkersRestartedAt::create();
+
+        Console::info(_l('Restart signal sent to queue workers'));
     }
 }
