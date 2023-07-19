2d41d1a2c4543aa0f7d12c227f36b732935c3647
jxtou <jxtou@xirelogy.com>
2023-07-19 16:15:44 +0800

    Change OptionsCollection's argument type

--- src/Commands/OptionsCollection.php
+++ src/Commands/OptionsCollection.php
@@ -7,5 +7,15 @@ namespace Magpie\Commands;
  */
 abstract class OptionsCollection extends Collection
 {
+    /**
+     * Constructor
+     * @param array $arr
+     * @param string|null $prefix
+     */
+    public function __construct(array $arr, ?string $prefix = null)
+    {
+        parent::__construct($arr, $prefix);
 
+        $this->argType = _l('option');
+    }
 }
